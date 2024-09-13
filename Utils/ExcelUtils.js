12a976e8b69586b08596d9f0e8cd6527f3503c9f
rashupk007 <ahmedrashaad@hotmail.com>
2024-09-13 17:11:06 +0530

    removed excel dependency and removed excel testdatarelated testcases due to some issue in jenkins

--- Utils/ExcelUtils.js
+++ Utils/ExcelUtils.js
@@ -1,18 +1,18 @@
-// const xlsx = require('xlsx')
-import xlsx from 'xlsx';
+// // const xlsx = require('xlsx')
+// import xlsx from 'xlsx';
 
-class ExcelUtils {
+// class ExcelUtils {
 
-    static getDataFromExcel(filePath, sheetName) {
+//     static getDataFromExcel(filePath, sheetName) {
 
-        const workbook = xlsx.readFile(filePath)
-        const sheet = workbook.Sheets[sheetName]
-        const excelToJson = xlsx.utils.sheet_to_json(sheet)
+//         const workbook = xlsx.readFile(filePath)
+//         const sheet = workbook.Sheets[sheetName]
+//         const excelToJson = xlsx.utils.sheet_to_json(sheet)
         
-        return excelToJson
+//         return excelToJson
 
-    }
+//     }
 
-}
+// }
 
-module.exports = {ExcelUtils}
+// module.exports = {ExcelUtils}
--- tests/LoginPageTestUsingExcelData.spec.js
+++ tests/LoginPageTestUsingExcelData.spec.js
@@ -1,27 +1,27 @@
-const {test, expect} = require('@playwright/test')
+// const {test, expect} = require('@playwright/test')
 
-const {LoginPage} = require('../pageObjects/LoginPage')
+// const {LoginPage} = require('../pageObjects/LoginPage')
 
-const {ExcelUtils} = require('../Utils/ExcelUtils')
+// const {ExcelUtils} = require('../Utils/ExcelUtils')
 
-let loginPage
-let testData
+// let loginPage
+// let testData
 
-const filePath = "E:\\Downloads\\Backup\\Rashaad_LP_BackUp\\Playwright_Automation_Learning\\PlaywrightAutomation\\TestData\\loginExcelData.xlsx"
+// const filePath = "E:\\Downloads\\Backup\\Rashaad_LP_BackUp\\Playwright_Automation_Learning\\PlaywrightAutomation\\TestData\\loginExcelData.xlsx"
 
-try{
-    testData = ExcelUtils.getDataFromExcel(filePath, "loginExcelData")
-} catch (error) {
-    console.log("File Not Found")
-}
+// try{
+//     testData = ExcelUtils.getDataFromExcel(filePath, "loginExcelData")
+// } catch (error) {
+//     console.log("File Not Found")
+// }
 
-for(const data of testData) {
-    test.beforeEach(async ({page})=>{
-        loginPage = new LoginPage(page)
-        await loginPage.launchApplication(data.url)
-    })
-    test(`Check the login with valid credentials for ${data.name}`, {tag:'@pom2'}, async ({})=>{
-        await loginPage.validLogin(data.username, data.password)
-        await expect(loginPage.homePageIdentifier).toBeVisible()
-    })
-}
+// for(const data of testData) {
+//     test.beforeEach(async ({page})=>{
+//         loginPage = new LoginPage(page)
+//         await loginPage.launchApplication(data.url)
+//     })
+//     test(`Check the login with valid credentials for ${data.name}`, {tag:'@pom2'}, async ({})=>{
+//         await loginPage.validLogin(data.username, data.password)
+//         await expect(loginPage.homePageIdentifier).toBeVisible()
+//     })
+// }
